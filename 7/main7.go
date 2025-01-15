2c3c7555b5a20f77c3a8ca4f7cbe11637a37509f
Nikolai Bazhenov <bjacknoah@gmail.com>
2025-01-15 22:09:26 +0500

    complete task 7

--- 7/main7.go
+++ 7/main7.go
@@ -1,1 +1,53 @@
 package main
+
+import "sync"
+
+func main() {
+	ch1, ch2, ch3 := make(chan int), make(chan int), make(chan int)
+
+	go func() {
+		for i := 0; i < 12; i++ {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+	go func() {
+		for i := 100; i < 135; i++ {
+			ch2 <- i
+		}
+		close(ch2)
+	}()
+	go func() {
+		for i := 1000; i < 1077; i++ {
+			ch3 <- i
+		}
+		close(ch3)
+	}()
+
+	for out := range mergeChannels(ch1, ch2, ch3) {
+		println(out)
+	}
+}
+
+func mergeChannels(channels ...<-chan int) <-chan int {
+	resultChannel := make(chan int)
+
+	wg := &sync.WaitGroup{}
+	for _, ch := range channels {
+		wg.Add(1)
+		go func(ch <-chan int) {
+			defer wg.Done()
+			for val := range ch {
+				resultChannel <- val
+			}
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChannel)
+	}()
+
+	return resultChannel
+
+}
--- 7/main7_test.go
+++ 7/main7_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestMergeChannels(t *testing.T) {
+
+	t.Run("merge channels", func(t *testing.T) {
+		ch1, ch2, ch3 := make(chan int), make(chan int), make(chan int)
+		go func() {
+			for i := 0; i < 15; i++ {
+				ch1 <- i
+			}
+			close(ch1)
+		}()
+		go func() {
+			for i := 0; i < 10; i++ {
+				ch2 <- i
+			}
+			close(ch2)
+		}()
+		go func() {
+			for i := 0; i < 5; i++ {
+				ch3 <- i
+			}
+			close(ch3)
+		}()
+
+		for out := range mergeChannels(ch1, ch2, ch3) {
+			t.Log(out)
+		}
+	})
+
+}
