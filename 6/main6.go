bb1384cd144dc7d4db116e26c177f9517bbb1f6d
Nikolai Bazhenov <bjacknoah@gmail.com>
2025-01-15 22:08:36 +0500

    complete task 6

--- 6/main6.go
+++ 6/main6.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	go getRandomIntNumber(ch)
+	for {
+		fmt.Println(<-ch)
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+func getRandomIntNumber(ch chan int) {
+	for {
+		ch <- rand.Int()
+	}
+}
--- 6/main6_test.go
+++ 6/main6_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestGetRandomIntNumber(t *testing.T) {
+	t.Run("GetRandomIntNumber", func(t *testing.T) {
+		ch := make(chan int)
+		go getRandomIntNumber(ch)
+		actual := <-ch
+		if actual == 0 {
+			t.Errorf("expected any int, got %v", actual)
+		}
+	})
+}
