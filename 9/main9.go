24a953420b345a512d0d5ec897bbd175e5b19705
Nikolai Bazhenov <bjacknoah@gmail.com>
2025-01-15 22:08:23 +0500

    complete task 9

--- 9/main9.go
+++ 9/main9.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan uint8)
+	ch2 := make(chan float64)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch1 <- uint8(i)
+			time.Sleep(3 * time.Second)
+		}
+		close(ch1)
+	}()
+
+	go conveer(ch1, ch2)
+	for val := range ch2 {
+		fmt.Println(val)
+	}
+}
+
+func conveer(ch1 chan uint8, ch2 chan float64) {
+	defer close(ch2)
+	for {
+		select {
+		case val, open := <-ch1:
+			if !open {
+				return
+			}
+			floatNum := float64(val)
+			ch2 <- math.Pow(floatNum, 2)
+		default:
+			continue
+		}
+
+	}
+}
--- 9/main9_test.go
+++ 9/main9_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConveer(t *testing.T) {
+	expected := []float64{0, 1, 4, 9, 16, 25, 36, 49, 64, 81}
+	t.Run("OK", func(t *testing.T) {
+		ch1 := make(chan uint8)
+		ch2 := make(chan float64)
+
+		go func() {
+			for i := 0; i < 10; i++ {
+				ch1 <- uint8(i)
+			}
+			close(ch1)
+		}()
+
+		go conveer(ch1, ch2)
+		actualSlice := []float64{}
+		for val := range ch2 {
+			if reflect.TypeOf(val).Kind() != reflect.Float64 {
+				t.Errorf("Wrong type. Expected float64, got %v", reflect.TypeOf(val).Kind())
+			}
+			actualSlice = append(actualSlice, val)
+		}
+		if !reflect.DeepEqual(expected, actualSlice) {
+			t.Errorf("Wrong result. Expected %v, got %v", expected, actualSlice)
+		}
+	})
+}
