31c653c3a1f9d4cfa946b21becc44e84bec891c5
Nikolai Bazhenov <bjacknoah@gmail.com>
2025-01-15 22:08:50 +0500

    complete task 5

--- 5/main5.go
+++ 5/main5.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+func main() {
+	a := []int{64, 2, 3, 43}
+	b := []int{64, 2, 3, 43}
+	hasIntersection, intersection := intersectionOfSlices(a, b)
+	fmt.Println(hasIntersection, intersection)
+}
+
+func intersectionOfSlices(a, b []int) (bool, []int) {
+	if len(a) == 0 || len(b) == 0 {
+		return false, []int{}
+	}
+	intersectionSlice := []int{}
+	intersectionMap := make(map[int]int)
+	for _, v := range a {
+		intersectionMap[v] = 1
+	}
+	for _, v := range b {
+		if _, ok := intersectionMap[v]; ok {
+			intersectionSlice = append(intersectionSlice, v)
+		}
+	}
+
+	if len(intersectionSlice) == 0 {
+		return false, intersectionSlice
+	}
+
+	return true, intersectionSlice
+}
--- 5/main5_test.go
+++ 5/main5_test.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntersectionOfSlices(t *testing.T) {
+	testTable := []struct {
+		name     string
+		slice1   []int
+		slice2   []int
+		expected struct {
+			hasIntersection bool
+			intersection    []int
+		}
+	}{
+		{
+			name:   "OK",
+			slice1: []int{1, 2, 3, 4, 5},
+			slice2: []int{2, 3, 4, 5, 6},
+			expected: struct {
+				hasIntersection bool
+				intersection    []int
+			}{
+				hasIntersection: true,
+				intersection:    []int{2, 3, 4, 5},
+			},
+		},
+		{
+			name:   "Empty slice",
+			slice1: []int{},
+			slice2: []int{2, 3, 4, 5, 6},
+			expected: struct {
+				hasIntersection bool
+				intersection    []int
+			}{
+				hasIntersection: false,
+				intersection:    []int{},
+			},
+		},
+		{
+			name:   "all Empty slices",
+			slice1: []int{},
+			slice2: []int{},
+			expected: struct {
+				hasIntersection bool
+				intersection    []int
+			}{
+				hasIntersection: false,
+				intersection:    []int{},
+			},
+		},
+	}
+	for _, trstCase := range testTable {
+		t.Run(trstCase.name, func(t *testing.T) {
+			hasInter, intersection := intersectionOfSlices(trstCase.slice1, trstCase.slice2)
+			if hasInter != trstCase.expected.hasIntersection {
+				t.Errorf("expected %v, got %v", trstCase.expected.hasIntersection, hasInter)
+			}
+			if !reflect.DeepEqual(intersection, trstCase.expected.intersection) {
+				t.Errorf("expected %v, got %v", trstCase.expected.intersection, intersection)
+			}
+		})
+
+	}
+}
