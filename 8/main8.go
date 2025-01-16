71e4ef64b0f0905a57a609801d389ca038a105bc
Nikolai Bazhenov <bjacknoah@gmail.com>
2025-01-16 15:53:47 +0500

    complete task 8

--- 8/main8.go
+++ 8/main8.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type WaitGroup struct {
+	sem     chan struct{}
+	counter int
+}
+
+func NewWaitGroup(max int) *WaitGroup {
+	return &WaitGroup{
+		sem: make(chan struct{}, max),
+	}
+}
+
+func (wg *WaitGroup) Add(count int) {
+	wg.counter += count
+	for i := 0; i < count; i++ {
+		wg.sem <- struct{}{}
+	}
+}
+
+func (wg *WaitGroup) Done() {
+	if wg.counter > 0 {
+		wg.counter--
+		<-wg.sem
+	}
+}
+
+func (wg *WaitGroup) Wait() {
+	for wg.counter > 0 {
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func main() {
+	wg := NewWaitGroup(100)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(10 * time.Millisecond)
+			fmt.Printf("Cogoroutine %d is done!\n", i)
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("All gorutines are done!")
+}
--- 8/main8_test.go
+++ 8/main8_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+func TestWaitGroup(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		wg := NewWaitGroup(100)
+		wg.Add(10)
+		if wg.counter != 10 {
+			t.Error("After wg.Add(10) counter is not 10")
+		}
+		wg.Done()
+		if wg.counter != 9 {
+			t.Error("After wg.Done() counter is not 9")
+		}
+	})
+}
